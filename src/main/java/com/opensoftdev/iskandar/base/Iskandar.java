6e3d1c3f7eab53c10d91735c9946ddba64ec39ae
Alastair Dawson <alastair.dawson@opensoftdev.com>
2010-06-15 17:10:21 -0700

    removed netbeans comments

--- src/main/java/com/opensoftdev/iskandar/base/Iskandar.java
+++ src/main/java/com/opensoftdev/iskandar/base/Iskandar.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.opensoftdev.iskandar.base;
 
 import com.opensoftdev.iskandar.core.ICommand;
@@ -10,13 +5,6 @@ import com.opensoftdev.iskandar.core.ICommandMap;
 import com.opensoftdev.iskandar.core.IEvent;
 import com.opensoftdev.iskandar.core.IEventDispatcher;
 
-
-/**
- *
- * @author arash
- *
- * 
- */
 public class Iskandar{
 
     private static Iskandar instance = null;
